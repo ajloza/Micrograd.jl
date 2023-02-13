155b37367f48bb11d379eaf8eddd8db87f67d6ae
Andrew Loza <77357202+ajloza@users.noreply.github.com>
2023-02-12 22:05:51 -0500

    initial commit

--- src/Micrograd.jl
+++ src/Micrograd.jl
@@ -1,5 +1,34 @@
 module Micrograd
 
 # Write your package code here.
+export Value
+
+mutable struct Value{T<:AbstractFloat}
+    data::T
+    grad::T
+    prev::Tuple
+    op::Char
+    backward::Function
+
+end
+
+function Value(d::T) where T
+    Value(d,zero(T),(),' ',()->nothing)
+end
+function Value(d::T,c::Tuple) where T
+    Value(d,zero(T),c,' ',()->nothing)
+end
+function Value(d::T,c::Tuple,o::Char) where T
+    Value(d,zero(T),c,o,()->nothing)
+end
+function Value(d::T,g::T,c::Tuple,o::Char) where T
+    Value(d,g,c,o,()->nothing)
+end
+
+Base.:+(x::Value{T},y::Value{T}) where T = Value(x.data+y.data,one(T),(x,y),'+')
+Base.:-(x::Value{T},y::Value{T}) where T = Value(x.data-y.data,one(T),(x,y),'-')
+Base.:*(x::Value{T},y::Value{T}) where T = Value(x.data*y.data,one(T),(x,y),'*')
+Base.:/(x::Value{T},y::Value{T}) where T = Value(x.data/y.data,one(T),(x,y),'/')
+Base.:^(x::Value{T},y::Value{T}) where T = Value(x.data^y.data,one(T),(x,y),'^')
 
 end
---
+++ src/type_dev.jl
@@ -0,0 +1,9 @@
+using Micrograd
+
+a = Value(1.0)
+b = Value(2.0)
+
+a+b
+a-b
+a*b
+a/b
