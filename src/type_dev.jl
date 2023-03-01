892f40d5a17a167314bc4acc90a0deab17148145
Andrew Loza <77357202+ajloza@users.noreply.github.com>
2023-02-28 22:51:36 -0500

    working on nn

--- src/type_dev.jl
+++ src/type_dev.jl
@@ -51,7 +51,46 @@ n = neuron(3)
 x = [1.0,2.0,1.0]
 o = n(x)
 
-nodes,depth = buildgraph(o)
+try
+    n([1.0,2.0])
+catch e
+    println(e)
+    println("correctly throws error")
+end
 
+# make a layer with 3 inputs and 10 outputs
+l = layer(3,10)
+l(x)
+
+# make a mlp with 3 inputs and 3 layers of size 4, 4, then 1
+m = mlp(3,[4,4,1])
+
+# don't try this is wasn't made for it
+o = m(x)
+nodes,depth = buildgraph(o)
 printgraph(nodes,depth)
-backward(o)
+
+# set up the binary classifier from Micrograd
+X = [
+    [2.0,3.0,-1.0],
+    [3.0,-1.0,0.5],
+    [0.5,1.0,1.0],
+    [1.0,1.0,-1.0],
+]
+
+y = [1.0,-1.0,-1.0,1.0]
+
+y_pred = m.(X)
+m_loss = loss(y,y_pred,"l2")
+
+# check that grads are zero
+m.layers[1].neurons[1].w[1]
+
+backward(m_loss)
+
+# see the new grad
+m.layers[1].neurons[1].w[1]
+
+parameters(n)
+parameters(l)
+p = parameters(m)
